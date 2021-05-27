695afc010698ec1ecde0ca7697772b681c28350a
nazeudon <nazeudon48112@icloud.com>
2021-05-27 23:26:13 +0900

    added Context component

--- pages/context-page.tsx
+++ pages/context-page.tsx
@@ -1,9 +1,16 @@
 import Layout from "../components/Layout";
+import ContextA from "../components/ContextA";
+import ContextB from "../components/ContextB";
+import { StateProvider } from "../context/StateProvider";
 
 const ContextPage: React.FC = () => {
   return (
     <Layout title="Context">
-      <p className="text-4xl">context page</p>
+      <p className="mb-10 text-4xl">context page</p>
+      <StateProvider>
+        <ContextA />
+        <ContextB />
+      </StateProvider>
     </Layout>
   );
 };
